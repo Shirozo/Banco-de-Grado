101e73f6621b44d7dd6096bdc73c28589058df31
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-10-21 21:18:44 +0800

    New db

---
+++ database/migrations/2024_10_21_121407_create_grades_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->float("first_sem", precision:2)->nullable();
+            $table->float("second_sem", precision:2)->nullable();
+            $table->foreignId("subject_id")->constrained("subjects")->onDelete("cascade")->onUpdate("cascade");
+            $table->foreignId("student_id")->constrained("users")->onDelete("cascade")->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
