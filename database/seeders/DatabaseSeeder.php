f2ef6d09cf0583d46b7e8d735fc0674b61c9b9a5
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-10-21 21:19:20 +0800

    modifies seeder' clear

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'Me',
+            'middle_name' => 'User',
             'email' => 'test@example.com',
         ]);
     }
