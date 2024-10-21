e15c26942418c1cfc1528e4daf9fd5ac177ca518
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-10-21 21:19:49 +0800

    imported new css and js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@ import './bootstrap';
 import "../../public/assets/jquery-3.7.1.min.js";
 import "../../public/assets/js/bootstrap.bundle.js"
 import "../../public/assets/js/dataTables.min.js";
+import "../../public/assets/js/select2.min.js";
