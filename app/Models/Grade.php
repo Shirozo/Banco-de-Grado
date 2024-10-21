6dca60f3a42c657733a4b2919344c1d1d6508706
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-10-21 21:17:05 +0800

    Database models

---
+++ app/Models/Grade.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    //
+
+    protected $fillable = [
+        "student_id",
+        "subject_id",
+        "first_sem",
+        "second_sem"
+    ];
+}
