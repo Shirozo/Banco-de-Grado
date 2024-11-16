93c719251765d303522025bde2d8af6b230994a6
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-11-16 21:34:02 +0800

    Register grade delete route and rename subject index route name:

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(["prefix" => "", "as" => "subject."], function () {
     Route::get('/', [SubjectController::class, 'show'])->name("index");
 
+    Route::get('/', [SubjectController::class, 'show'])->name("show");
+
     Route::post('/add/subject', [SubjectController::class, 'store'])->name("store");
 
     Route::put("/update/subject", [SubjectController::class, 'update'])->name("update");
@@ -24,6 +26,8 @@ Route::group(["prefix" => "grades", "as" => "grade."], function () {
     Route::post("/enroll/student", [GradeController::class, "store"])->name("store");
 
     Route::put('/grades/update', [GradeController::class, "update"])->name("update");
+
+    Route::delete('/grades/id', [GradeController::class, "delete"])->name("destroy");
 
     Route::get("/find/id", [GradeController::class, "api"])->name("api");
 });
