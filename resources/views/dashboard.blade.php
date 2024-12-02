a7d3f279c82d7f73546582744c453b0be6bc1d74
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-03 00:36:54 +0800

    color correction

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
-                                        <h5 class="card-title text-uppercase mb-0">Subjects</h5>
+                                        <h5 class="card-title text-uppercase mb-0" style="color:green !important;">Subjects</h5>
                                         <span class="h2 font-weight-bold mb-0">0</span>
                                     </div>
                                 </div>
@@ -31,7 +31,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
-                                        <h5 class="card-title text-uppercase  mb-0">Enrolled Student</h5>
+                                        <h5 class="card-title text-uppercase  mb-0" style="color:green !important;">Enrolled Student</h5>
                                         <span class="h2 font-weight-bold mb-0">0</span>
                                     </div>
                                 </div>
