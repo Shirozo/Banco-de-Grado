1d83987a146285b49e447aabe203d0d33462b796
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-07 23:34:29 +0800

    user ajax for login request

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,6 +10,7 @@
     </title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
+    <script src="{{ asset('assets/js/sweetalert.min.js') }}"></script>
 
 </head>
 
@@ -20,18 +21,18 @@
         </div>
     </nav>
     <div class="login-main-content">
-        <form class="login-form" action="{{ route('login') }}" method="POST">
+        <form class="login-form" action="{{ route('login') }}" method="POST" id="login-form">
             @csrf
             <div class="login-title">
                 <h1>LOG IN</h1>
             </div>
             <div class="form-field">
                 <label for="username">Username:</label>
-                <input type="text" name="username" required class="form-control" placeholder="Enter your username">
+                <input type="text" name="username" id="username" required class="form-control" placeholder="Enter your username">
             </div>
             <div class="form-field">
                 <label for="password">Password:</label>
-                <input type="password" name="password" required class="form-control" placeholder="Enter your password">
+                <input type="password" name="password" id="password" required class="form-control" placeholder="Enter your password">
             </div>
             <button type="submit" class="form-control form-submit">
                 <span>LOG IN</span>
@@ -39,6 +40,33 @@
         </form>
     </div>
     <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            $("#login-form").on("submit", function(e){
+                e.preventDefault();
+                $.ajax({
+                    type: "POST",
+                    url: "{{ route('login') }}",
+                    data: {
+                        _token: "{{ csrf_token() }}",
+                        username: $("#username").val(),
+                        password: $("#password").val()
+                    },
+                    success: function(response) {
+                        window.location.reload()
+                    },
+                    error: function(errr) {
+                        swal({
+                            title: "Error",
+                            text: "Invalid Credentials!",
+                            icon: "error",
+                            button: "Close"
+                        })
+
+                    }
+                })
+                
+            })
+        })
     </script>
 </body>
 
