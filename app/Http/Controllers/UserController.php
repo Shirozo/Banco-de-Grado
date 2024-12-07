65c12335502d34744885ec555c8d3a6a337099c1
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-07 23:04:06 +0800

    Implemented Backend for User CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,160 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function show(Request $request) {
-        return view("users");
+    public function show(Request $request)
+    {
+
+        $users = User::all();
+        return view("users", [
+            "users" => $users
+        ]);
     }
+
+    public function store(Request $request)
+    {
+        try {
+
+            $data = Validator::make($request->all(), [
+                "name" => "required|min:1",
+                "username" => "required|max:30",
+                "password" => "required|min:8|max:30",
+                "type" => "required|integer|in:1,2"
+            ]);
+
+            if ($data->fails()) {
+                return response()->json([
+                    "message" => "Data validation fails!"
+                ], 403);
+            }
+
+            $has_data = User::where("username", "=", $request->username)->first();
+
+            if ($has_data) {
+                return response()->json([
+                    "message" => "Username already taken!"
+                ], 403);
+            }
+
+            User::create([
+                "name" => $request->name,
+                "username" => $request->username,
+                "password" => $request->password,
+                "type" => $request->type
+            ]);
+
+            return response()->json([
+                "message" => "User Added!"
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        if ($request->has("delete_id")) {
+            $data = User::find($request->delete_id);
+
+            if ($data) {
+                $data->delete();
+
+                return response()->json([
+                    "message" => "User Deleted!"
+                ], 200);
+            }
+
+            return response()->json([
+                "message" => "User data not found!"
+            ], 404);
+        }
+        return response()->json([
+            "message" => "ID is required!"
+        ], 403);
+    }
+
+    public function update(Request $request)
+    {
+        try {
+
+            if (!$request->has("id")) {
+                return response()->json([
+                    "message" => "ID not found!"
+                ], 404);
+            }
+
+            $data = Validator::make($request->all(), [
+                "update_name" => "required|min:1",
+                "update_username" => "required|max:30",
+                "update_password" => "max:30",
+                "update_type" => "required|integer|in:1,2"
+            ]);
+
+            if ($data->fails()) {
+                return response()->json([
+                    "message" => "Form validation fails!"
+                ], 403);
+            }
+
+            $prev_data = User::find($request->id);
+
+            if (!$prev_data) {
+                return response()->json([
+                    "message" => "User data not found!"
+                ], 404);
+            }
+
+            if (strlen($request->update_password) >= 8) {
+                $prev_data->update([
+                    "name" => $request->update_name,
+                    "username" => $request->update_username,
+                    "password" => $request->update_password,
+                    "type" => $request->update_type
+                ]);
+            } else {
+                $prev_data->update([
+                    "name" => $request->update_name,
+                    "username" => $request->update_username,
+                    "type" => $request->update_type
+                ]);
+            }
+
+            return response()->json([
+                "message" => "User data updated!"
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function userDataApi(Request $request)
+    {
+        if ($request->has("id")) {
+            $data = User::find($request->id);
+
+            if ($data) {
+                return response()->json([
+                    "data" => $data
+                ], 200);
+            }
+
+            return response()->json([
+                "message" => "User Data not found!"
+            ], 404);
+        }
+        return response()->json([
+            "message" => "ID is required!"
+        ], 404);
+    }
+
+
 
     public function api(Request $request)
     {
