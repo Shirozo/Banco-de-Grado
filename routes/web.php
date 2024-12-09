ffb7ee54acd7cd4c0413bf3e8c33444071bde193
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-10 01:13:03 +0800

    route redesign

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "", "as" => "subject.", "middleware" => ['auth']], function () {
+Route::get("", );
+
+Route::group(["prefix" => "subject", "as" => "subject.", "middleware" => ['auth']], function () {
 
     Route::get('/', [SubjectController::class, 'show'])->name("show");
 
-    Route::post('/add/subject', [SubjectController::class, 'store'])->name("store");
+    Route::post('/add', [SubjectController::class, 'store'])->name("store");
 
-    Route::put("/update/subject", [SubjectController::class, 'update'])->name("update");
+    Route::put("/update", [SubjectController::class, 'update'])->name("update");
 
-    Route::delete("/delete/subject", [SubjectController::class, 'destroy'])->name("destroy");
+    Route::delete("/delete", [SubjectController::class, 'destroy'])->name("destroy");
+
+    Route::get("/generate/report/excel/id/{id}", [SubjectController::class, "generateReport"])->name("report");
 });
 
 
@@ -53,17 +57,17 @@ Route::group(["prefix" => "student", "as" => "student.", "middleware" => ['auth'
 
     Route::get("/", [StudentController::class, "show"])->name("show");
     
+    Route::post("/store/data", [StudentController::class, "store"])->name("store");
+
+    Route::put('/update/data', [StudentController::class, "update"])->name("update");
+
+    Route::delete("/delete/data", [StudentController::class, "destroy"])->name("destroy");
+
     Route::get("/find/id", [StudentController::class, "api"])->name("api");
 
     Route::get("/data/per/subject", [StudentController::class, "dataApi"])->name("dataApi");
 
     Route::get("/data/all", [StudentController::class, "all"])->name("all");
-
-    Route::post("/store/data", [StudentController::class, "store"])->name("store");
-
-    Route::delete("/delete/data", [StudentController::class, "destroy"])->name("destroy");
-
-    Route::put('/update/data', [StudentController::class, "update"])->name("update");
 
     Route::post("/upload/data", [StudentController::class, "upload"])->name("upload");
 });
