51f9642999b76d5d182526168133a223c72021a8
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-20 14:24:06 +0800

    1st sem -> midterm and 2nd sem -> Final

--- database/migrations/2024_10_27_144314_create_grades_table.php
+++ database/migrations/2024_10_27_144314_create_grades_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->float("first_sem", precision:2)->nullable();
-            $table->float("second_sem", precision:2)->nullable();
+            $table->char("midterm",3)->nullable();
+            $table->char("final", 3)->nullable();
             $table->enum("status", ["active", "dropped"]);
             $table->foreignId("subject_id")->constrained("subjects")->onDelete("cascade")->onUpdate("cascade");
             $table->foreignId("student_id")->constrained("students")->onDelete("cascade")->onUpdate("cascade");
